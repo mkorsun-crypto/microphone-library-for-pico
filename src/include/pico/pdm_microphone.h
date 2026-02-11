f1ebba4898dbd09d7d1fb1c2e1d3f696480e91fc
mkorsun-crypto <m.korsun@kre.dp.ua>
2026-02-11 23:26:48 +0200

    Update pdm_microphone.h

--- src/include/pico/pdm_microphone.h
+++ src/include/pico/pdm_microphone.h
@@ -1,37 +1,36 @@
 /*
  * Copyright (c) 2021 Arm Limited and Contributors. All rights reserved.
- *
  * SPDX-License-Identifier: Apache-2.0
- * 
  */
 
 #ifndef _PICO_PDM_MICROPHONE_H_
 #define _PICO_PDM_MICROPHONE_H_
 
 #include "hardware/pio.h"
+#include "pico/stdlib.h"
 
-typedef void (*pdm_samples_ready_handler_t)(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+// Настройки микрофона (I2S disguised as PDM)
 struct pdm_microphone_config {
     uint gpio_data;
-    uint gpio_clk;
+    uint gpio_clk; // Это будет BCLK (SCK)
+    uint gpio_ws;  // НОВОЕ: Это будет LRCLK (WS)
     PIO pio;
     uint pio_sm;
     uint sample_rate;
     uint sample_buffer_size;
 };
 
-int pdm_microphone_init(const struct pdm_microphone_config* config);
-void pdm_microphone_deinit();
-
-int pdm_microphone_start();
-void pdm_microphone_stop();
-
-void pdm_microphone_set_samples_ready_handler(pdm_samples_ready_handler_t handler);
-void pdm_microphone_set_filter_max_volume(uint8_t max_volume);
-void pdm_microphone_set_filter_gain(uint8_t gain);
-void pdm_microphone_set_filter_volume(uint16_t volume);
-
+int pdm_microphone_init(const struct pdm_microphone_config *config);
+void pdm_microphone_start(void);
 int pdm_microphone_read(int16_t* buffer, size_t samples);
+void pdm_microphone_set_samples_ready_handler(void (*handler)(void));
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
