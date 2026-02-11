adf66a9885119f8f768677e9f4a0cde39186bf09
mkorsun-crypto <m.korsun@kre.dp.ua>
2026-02-11 23:28:14 +0200

    Update pdm_microphone.c

--- src/pdm_microphone.c
+++ src/pdm_microphone.c
@@ -1,251 +1,120 @@
 /*
- * Copyright (c) 2021 Arm Limited and Contributors. All rights reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- * 
+ * I2S Microphone Driver (Imposter as PDM)
  */
 
-#include <stdlib.h>
 #include <string.h>
-
-#include "hardware/clocks.h"
+#include "pico/pdm_microphone.h"
+#include "pdm_microphone.pio.h"
 #include "hardware/dma.h"
 #include "hardware/irq.h"
 
-#include "OpenPDM2PCM/OpenPDMFilter.h"
+// Variables
+static struct pdm_microphone_config _config;
+static uint _dma_channel;
+static int32_t* _raw_buffer;
+static void (*_samples_ready_handler)(void) = NULL;
 
-#include "pdm_microphone.pio.h"
+// PIO Configuration
+static void pdm_microphone_init_pio() {
+    uint offset = pio_add_program(_config.pio, &pdm_microphone_data_program);
+    pio_sm_config c = pdm_microphone_data_program_get_default_config(offset);
 
-#include "pico/pdm_microphone.h"
+    // Pin setup
+    // Side-set pins (SCK, WS)
+    sm_config_set_sideset_pins(&c, _config.gpio_clk); 
+    // Input pin (DATA)
+    sm_config_set_in_pins(&c, _config.gpio_data);
+    
+    // Init GPIO
+    pio_gpio_init(_config.pio, _config.gpio_clk);
+    pio_gpio_init(_config.pio, _config.gpio_ws);
+    pio_gpio_init(_config.pio, _config.gpio_data);
+    
+    // Set directions
+    pio_sm_set_consecutive_pindirs(_config.pio, _config.pio_sm, _config.gpio_clk, 2, true); // SCK, WS output
+    pio_sm_set_consecutive_pindirs(_config.pio, _config.pio_sm, _config.gpio_data, 1, false); // Data input
 
-#define PDM_DECIMATION       64
-#define PDM_RAW_BUFFER_COUNT 2
+    // Shift settings: INMP441 sends MSB first.
+    // We shift Left, Autopush enabled at 32 bits
+    sm_config_set_in_shift(&c, false, true, 32); 
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
 
-static struct {
-    struct pdm_microphone_config config;
-    int dma_channel;
-    uint8_t* raw_buffer[PDM_RAW_BUFFER_COUNT];
-    volatile int raw_buffer_write_index;
-    volatile int raw_buffer_read_index;
-    uint raw_buffer_size;
-    uint dma_irq;
-    TPDMFilter_InitStruct filter;
-    uint16_t filter_volume;
-    pdm_samples_ready_handler_t samples_ready_handler;
-} pdm_mic;
+    // Clock divider calculation
+    // We need 64 clocks per sample (32 bits * 2 channels)
+    float div = clock_get_hz(clk_sys) / (_config.sample_rate * 64.0f);
+    sm_config_set_clkdiv(&c, div);
 
-static void pdm_dma_handler();
+    pio_sm_init(_config.pio, _config.pio_sm, offset, &c);
+}
 
-int pdm_microphone_init(const struct pdm_microphone_config* config) {
-    memset(&pdm_mic, 0x00, sizeof(pdm_mic));
-    memcpy(&pdm_mic.config, config, sizeof(pdm_mic.config));
-
-    if (config->sample_buffer_size % (config->sample_rate / 1000)) {
-        return -1;
+// DMA Handler
+void __isr dma_handler() {
+    dma_hw->ints0 = 1u << _dma_channel; // Clear interrupt
+    if (_samples_ready_handler) {
+        _samples_ready_handler();
     }
+    // Re-trigger DMA
+    dma_channel_set_write_addr(_dma_channel, _raw_buffer, true);
+}
 
-    pdm_mic.raw_buffer_size = config->sample_buffer_size * (PDM_DECIMATION / 8);
+int pdm_microphone_init(const struct pdm_microphone_config *config) {
+    memcpy(&_config, config, sizeof(struct pdm_microphone_config));
+    
+    // Allocate internal buffer for raw 32-bit I2S data (Stereo)
+    // Size is * 2 because we get L and R channels
+    _raw_buffer = malloc(_config.sample_buffer_size * 2 * sizeof(int32_t));
+    if (_raw_buffer == NULL) return -1;
 
-    for (int i = 0; i < PDM_RAW_BUFFER_COUNT; i++) {
-        pdm_mic.raw_buffer[i] = malloc(pdm_mic.raw_buffer_size);
-        if (pdm_mic.raw_buffer[i] == NULL) {
-            pdm_microphone_deinit();
+    pdm_microphone_init_pio();
 
-            return -1;   
-        }
-    }
-
-    pdm_mic.dma_channel = dma_claim_unused_channel(true);
-    if (pdm_mic.dma_channel < 0) {
-        pdm_microphone_deinit();
-
-        return -1;
-    }
-
-    uint pio_sm_offset = pio_add_program(config->pio, &pdm_microphone_data_program);
-
-    float clk_div = clock_get_hz(clk_sys) / (config->sample_rate * PDM_DECIMATION * 4.0);
-
-    pdm_microphone_data_init(
-        config->pio,
-        config->pio_sm,
-        pio_sm_offset,
-        clk_div,
-        config->gpio_data,
-        config->gpio_clk
-    );
-
-    dma_channel_config dma_channel_cfg = dma_channel_get_default_config(pdm_mic.dma_channel);
-
-    channel_config_set_transfer_data_size(&dma_channel_cfg, DMA_SIZE_8);
-    channel_config_set_read_increment(&dma_channel_cfg, false);
-    channel_config_set_write_increment(&dma_channel_cfg, true);
-    channel_config_set_dreq(&dma_channel_cfg, pio_get_dreq(config->pio, config->pio_sm, false));
-
-    pdm_mic.dma_irq = DMA_IRQ_0;
+    // DMA Setup
+    _dma_channel = dma_claim_unused_channel(true);
+    dma_channel_config c = dma_channel_get_default_config(_dma_channel);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
+    channel_config_set_read_increment(&c, false); // Read from PIO FIFO (fixed addr)
+    channel_config_set_write_increment(&c, true); // Write to buffer
+    channel_config_set_dreq(&c, pio_get_dreq(_config.pio, _config.pio_sm, false));
 
     dma_channel_configure(
-        pdm_mic.dma_channel,
-        &dma_channel_cfg,
-        pdm_mic.raw_buffer[0],
-        &config->pio->rxf[config->pio_sm],
-        pdm_mic.raw_buffer_size,
-        false
+        _dma_channel,
+        &c,
+        _raw_buffer,
+        &_config.pio->rxf[_config.pio_sm],
+        _config.sample_buffer_size * 2, // Count
+        false // Don't start yet
     );
 
-    pdm_mic.filter.Fs = config->sample_rate;
-    pdm_mic.filter.LP_HZ = config->sample_rate / 2;
-    pdm_mic.filter.HP_HZ = 10;
-    pdm_mic.filter.In_MicChannels = 1;
-    pdm_mic.filter.Out_MicChannels = 1;
-    pdm_mic.filter.Decimation = PDM_DECIMATION;
-    pdm_mic.filter.MaxVolume = 64;
-    pdm_mic.filter.Gain = 16;
+    // Enable IRQ
+    dma_channel_set_irq0_enabled(_dma_channel, true);
+    irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
+    irq_set_enabled(DMA_IRQ_0, true);
 
-    pdm_mic.filter_volume = pdm_mic.filter.MaxVolume;
+    return 0;
 }
 
-void pdm_microphone_deinit() {
-    for (int i = 0; i < PDM_RAW_BUFFER_COUNT; i++) {
-        if (pdm_mic.raw_buffer[i]) {
-            free(pdm_mic.raw_buffer[i]);
-
-            pdm_mic.raw_buffer[i] = NULL;
-        }
-    }
-
-    if (pdm_mic.dma_channel > -1) {
-        dma_channel_unclaim(pdm_mic.dma_channel);
-
-        pdm_mic.dma_channel = -1;
-    }
+void pdm_microphone_start(void) {
+    dma_channel_start(_dma_channel);
+    pio_sm_set_enabled(_config.pio, _config.pio_sm, true);
 }
 
-int pdm_microphone_start() {
-    irq_set_enabled(pdm_mic.dma_irq, true);
-    irq_set_exclusive_handler(pdm_mic.dma_irq, pdm_dma_handler);
-
-    if (pdm_mic.dma_irq == DMA_IRQ_0) {
-        dma_channel_set_irq0_enabled(pdm_mic.dma_channel, true);
-    } else if (pdm_mic.dma_irq == DMA_IRQ_1) {
-        dma_channel_set_irq1_enabled(pdm_mic.dma_channel, true);
-    } else {
-        return -1;
-    }
-
-    Open_PDM_Filter_Init(&pdm_mic.filter);
-
-    pio_sm_set_enabled(
-        pdm_mic.config.pio,
-        pdm_mic.config.pio_sm,
-        true
-    );
-
-    pdm_mic.raw_buffer_write_index = 0;
-    pdm_mic.raw_buffer_read_index = 0;
-
-    dma_channel_transfer_to_buffer_now(
-        pdm_mic.dma_channel,
-        pdm_mic.raw_buffer[0],
-        pdm_mic.raw_buffer_size
-    );
-
-    pio_sm_set_enabled(
-        pdm_mic.config.pio,
-        pdm_mic.config.pio_sm,
-        true
-    );
-}
-
-void pdm_microphone_stop() {
-    pio_sm_set_enabled(
-        pdm_mic.config.pio,
-        pdm_mic.config.pio_sm,
-        false
-    );
-
-    dma_channel_abort(pdm_mic.dma_channel);
-
-    if (pdm_mic.dma_irq == DMA_IRQ_0) {
-        dma_channel_set_irq0_enabled(pdm_mic.dma_channel, false);
-    } else if (pdm_mic.dma_irq == DMA_IRQ_1) {
-        dma_channel_set_irq1_enabled(pdm_mic.dma_channel, false);
-    }
-
-    irq_set_enabled(pdm_mic.dma_irq, false);
-}
-
-static void pdm_dma_handler() {
-    // clear IRQ
-    if (pdm_mic.dma_irq == DMA_IRQ_0) {
-        dma_hw->ints0 = (1u << pdm_mic.dma_channel);
-    } else if (pdm_mic.dma_irq == DMA_IRQ_1) {
-        dma_hw->ints1 = (1u << pdm_mic.dma_channel);
-    }
-
-    // get the current buffer index
-    pdm_mic.raw_buffer_read_index = pdm_mic.raw_buffer_write_index;
-
-    // get the next capture index to send the dma to start
-    pdm_mic.raw_buffer_write_index = (pdm_mic.raw_buffer_write_index + 1) % PDM_RAW_BUFFER_COUNT;
-
-    // give the channel a new buffer to write to and re-trigger it
-    dma_channel_transfer_to_buffer_now(
-        pdm_mic.dma_channel,
-        pdm_mic.raw_buffer[pdm_mic.raw_buffer_write_index],
-        pdm_mic.raw_buffer_size
-    );
-
-    if (pdm_mic.samples_ready_handler) {
-        pdm_mic.samples_ready_handler();
-    }
-}
-
-void pdm_microphone_set_samples_ready_handler(pdm_samples_ready_handler_t handler) {
-    pdm_mic.samples_ready_handler = handler;
-}
-
-void pdm_microphone_set_filter_max_volume(uint8_t max_volume) {
-    pdm_mic.filter.MaxVolume = max_volume;
-}
-
-void pdm_microphone_set_filter_gain(uint8_t gain) {
-    pdm_mic.filter.Gain = gain;
-}
-
-void pdm_microphone_set_filter_volume(uint16_t volume) {
-    pdm_mic.filter_volume = volume;
+void pdm_microphone_set_samples_ready_handler(void (*handler)(void)) {
+    _samples_ready_handler = handler;
 }
 
 int pdm_microphone_read(int16_t* buffer, size_t samples) {
-    int filter_stride = (pdm_mic.filter.Fs / 1000);
-    samples = (samples / filter_stride) * filter_stride;
-
-    if (samples > pdm_mic.config.sample_buffer_size) {
-        samples = pdm_mic.config.sample_buffer_size;
+    // Convert 32-bit I2S Stereo to 16-bit Mono (or Stereo)
+    // INMP441 data is high-aligned in the 32-bit word.
+    // L channel is at even indices, R at odd.
+    // Let's just take Left channel for now.
+    
+    for (size_t i = 0; i < samples; i++) {
+        int32_t raw_val = _raw_buffer[i * 2]; // Left channel
+        // Shift down to 16 bit. 
+        // Data is usually 24-bit left aligned in 32-bit frame.
+        // So bits [31:8] are valid.
+        // We want int16, so we take top 16 bits: [31:16].
+        // Simple right shift by 16.
+        buffer[i] = (int16_t)(raw_val >> 16); 
     }
-
-    if (pdm_mic.raw_buffer_write_index == pdm_mic.raw_buffer_read_index) {
-        return 0;
-    }
-
-    uint8_t* in = pdm_mic.raw_buffer[pdm_mic.raw_buffer_read_index];
-    int16_t* out = buffer;
-
-    pdm_mic.raw_buffer_read_index++;
-
-    for (int i = 0; i < samples; i += filter_stride) {
-#if PDM_DECIMATION == 64
-        Open_PDM_Filter_64(in, out, pdm_mic.filter_volume, &pdm_mic.filter);
-#elif PDM_DECIMATION == 128
-        Open_PDM_Filter_128(in, out, pdm_mic.filter_volume, &pdm_mic.filter);
-#else
-        #error "Unsupported PDM_DECIMATION value!"
-#endif
-
-        in += filter_stride * (PDM_DECIMATION / 8);
-        out += filter_stride;
-    }
-
     return samples;
 }
